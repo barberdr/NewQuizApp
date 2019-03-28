6aba95c2911e511464edd33eb14b4d7b9756b885
DB <barber.drb@outlook.com>
2019-03-27 21:04:44 -0700

    Random commit

--- Program.cs
+++ Program.cs
@@ -6,18 +6,21 @@ namespace NewQuizApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the quizz app! Enter 1 if you are ready for a quiz, or 2 if you would like to exit");
-            var UserInput = Console.ReadLine();
-            switch (UserInput)
-            {
-                case "1":
-                    //Run the quizz app
-                    break;
-                case "2":
-                    Console.WriteLine("Okay feel free to come back anytime!");
-                    break;
-                default:
-                    break;
+            while (true)
+	        {
+                Console.WriteLine("Welcome to the quizz app! Enter 0 if you are ready for a quiz, or 1 if you would like to exit");
+                var UserInput = Console.ReadLine();
+                switch (UserInput)
+                {
+                    case "0":
+                        //Run the quizz app
+                        break;
+                    case "1":
+                        Console.WriteLine("Okay feel free to come back anytime!");
+                        return;
+                    default:
+                        break;
+                }
             }
         }
     }
